d6c8c17f339c043e290bb23e82ebaf892306a676
dogeowsky <77202347+Dogeowsky@users.noreply.github.com>
2025-03-22 14:53:38 +0100

    Update mapchange.lua

--- server_scripts/mapchange.lua
+++ server_scripts/mapchange.lua
@@ -1,36 +1,59 @@
 -- dziwne to glua
 
---timer.Simple(5, function() 
-    --local plyNum = #player.getAll()
-    
-    mapPool =  {
-        "ttt_67thway_v5_2014",
-        "ttt_abyss",
-        "ttt_airbus_b3",
-        "ttt_airship_toppat",
-        "ttt_arctic_hl2",
-        "ttt_casino_b2",
-        "ttt_clue_2018_b7",
-        "ttt_clueless_rv",
-        "ttt_desperados",
-        "ttt_grovestreet_a13",
-        "ttt_minecraft_b5",
-        "ttt_minecraftcity_v4",
-        "ttt_overwatch",
-        "ttt_pelicantown",
-        "ttt_plaza_b7",
-        "ttt_rooftops_2016_v1",
-        "ttt_skyscraper",
-        "ttt_terrortrain_2019_b1",
-        "ttt_tokyodistrict",
-        "ttt_waterworld_tropical",
-        "ttt_westward"      
-    }   
+--maksymalna liczba zapisanych map (i tak trzeba or zedytować w while za 51 linijką)
+maxCachedMaps = 3
+--pula map
+mapPool =  {
+    "ttt_67thway_v5_2014",
+    "ttt_abyss",
+    "ttt_airbus_b3",
+    "ttt_airship_toppat",
+    "ttt_arctic_hl2",
+    "ttt_casino_b2",
+    "ttt_clue_2018_b7",
+    "ttt_clueless_rv",
+    "ttt_desperados",
+    "ttt_grovestreet_a13",
+    "ttt_minecraft_b5",
+    "ttt_minecraftcity_v4",
+    "ttt_overwatch",
+    "ttt_pelicantown",
+    "ttt_plaza_b7",
+    "ttt_rooftops_2016_v1",
+    "ttt_skyscraper",
+    "ttt_terrortrain_2019_b1",
+    "ttt_tokyodistrict",
+    "ttt_waterworld_tropical",
+    "ttt_westward"
+}
+
+-- kod
+timer.Simple(0, function()
+    lastMaps = string.Explode("\n", file.Read("lastmaps.txt"))
+
+    if #lastMaps >= maxCachedMaps then
+        file.Write("lastmaps.txt", "")
+        for i = 1, maxCachedMaps do
+            if i == maxCachedMaps then
+                lastMaps[i] = game.GetMap()
+            else
+                lastMaps[i] = lastMaps[i + 1]
+            end
+
+            file.Append("lastmaps.txt", lastMaps[i] .. "\n")
+        end
+    else
+        file.Append("lastmaps.txt", game.GetMap() .. "\n")
+    end
 
     nextMap = mapPool[math.random(1, #mapPool)]
 
-    while nextMap == game.GetMap() do
+    while nextMap == lastMaps[1]
+        or nextMap == lastMaps[2]
+        or nextMap == lastMaps[3]
+    do
         nextMap = mapPool[math.random(1, #mapPool)]
     end
 
     RunConsoleCommand("nextlevel", nextMap)
+end)
