2285babff25a8db529aade82ee6fd1193ef208e0
dogeowsky <77202347+Dogeowsky@users.noreply.github.com>
2025-03-23 19:49:54 +0100

    Added author comments in mapchange.lua

--- server_scripts/mapchange.lua
+++ server_scripts/mapchange.lua
@@ -1,6 +1,9 @@
+-- by Pompin84/Dogeowsky
 -- dziwne to glua
 
+-- Maximum number of maps stored in the file
 maxCachedMaps = 3
+-- The pool of maps on the server from which the next map will be drawn
 mapPool =  {
     "ttt_67thway_v5_2014",
     "ttt_abyss",
@@ -26,26 +29,33 @@ mapPool =  {
 }
 
 timer.Simple(0, function()
+    -- Loading saved maps into the table (if the file exists)
     lastMaps = string.Explode("\n", file.Read("lastmaps.txt"))
 
+    -- Removal of empty entries
     for i = #lastMaps, 1, -1 do
         if lastMaps[i] == "" then
             table.remove(lastMaps, i)
         end
     end
 
+    -- Inserting the current map to the end of the table
     table.insert(lastMaps, game.GetMap())
 
+    -- Removing the first entry until the number of entries is less than the maxCachedMaps
     while #lastMaps > maxCachedMaps do
         table.remove(lastMaps, 1)
     end
 
+    -- Saving a modified list in a file
     file.Write("lastmaps.txt", table.concat(lastMaps, "\n"))
 
+    -- Selecting a new map with avoidance of previous one
     local nextMap
     repeat
         nextMap = mapPool[math.random(1, #mapPool)]
     until not table.HasValue(lastMaps, nextMap)
 
+    -- Set the new map as the next
     RunConsoleCommand("nextlevel", nextMap)
 end)
